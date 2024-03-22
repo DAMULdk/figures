1b01de683a994a07a1cc4707fa5ce36a8f70c5d0
DAMULdk <damien2006.dk@gmail.com>
2024-03-22 21:15:34 +0100

    Add new figures, bug fixes

--- ecommerce/figures/urls.py
+++ ecommerce/figures/urls.py
@@ -3,7 +3,14 @@ from . import views
 
 urlpatterns = {
     path('', views.menu, name="menu"),
+    path('menu/', views.menu, name="menu"),
     path('square/', views.square, name="square"),
     path('triangle/', views.triangle, name="triangle"),
+    path('circle/', views.circle, name="circle"),
+    path('rectangle/', views.rectangle, name="rectangle"),
     path('cube/', views.cube, name="cube"),
+    path('sphere/', views.sphere, name="sphere"),
+    path('prism/', views.prism, name="prism"),
+    path('tesseract/', views.tesseract, name="tesseract"),
+    path('human/', views.human, name="human"),
 }
--- ecommerce/figures/views.py
+++ ecommerce/figures/views.py
@@ -12,6 +12,30 @@ def triangle(request):
     context = {}
     return render(request, 'figures/triangle.html', context)
 
+def circle(request):
+    context = {}
+    return render(request, 'figures/circle.html', context)
+
+def rectangle(request):
+    context = {}
+    return render(request, 'figures/rectangle.html', context)
+
 def cube(request):
     context = {}
     return render(request, 'figures/cube.html', context)
+
+def sphere(request):
+    context = {}
+    return render(request, 'figures/sphere.html', context)
+
+def prism(request):
+    context = {}
+    return render(request, 'figures/prism.html', context)
+
+def tesseract(request):
+    context = {}
+    return render(request, 'figures/tesseract.html', context)
+
+def human(request):
+    context = {}
+    return render(request, 'figures/human.html', context)
